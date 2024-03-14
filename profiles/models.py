8eea7548725bc6000d3a8d5c2a024982ec8e7ae3
Nelson Mehlis <128640902+ThePyth0nKid@users.noreply.github.com>
2024-03-14 13:01:05 +0100

    Add Profile model and migrations

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,11 +10,18 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_v3n3or'
-        )
-    
+        upload_to='images/', default='../default_profile_l4brwi'
+    )
+
     class Meta:
         ordering = ['-created_at']
-    
+
     def __str__(self):
-        return f"{self.owner}`s profile"
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
