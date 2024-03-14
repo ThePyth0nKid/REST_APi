f6cd61b728a85e707c22f4deb0a825381ffe6a25
Nelson Mehlis <128640902+ThePyth0nKid@users.noreply.github.com>
2024-03-14 15:05:06 +0100

    Add is_owner field to ProfileSerializer and pass request context to serializer in views.py

--- profiles/views.py
+++ profiles/views.py
@@ -9,7 +9,7 @@ from rest_api.permissions import IsOwnerOrReadOnly
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
         return Response(serializer.data)
     
 class ProfileDetail(APIView):
@@ -24,13 +24,13 @@ class ProfileDetail(APIView):
     def get(self, request, pk):
         profile = self.get_object(pk)
         self.check_object_permissions(self.request, profile)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile, context={'request': request})
         return Response(serializer.data)
     
     
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
